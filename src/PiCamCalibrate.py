4ce7ce64919494a966e215753dc73c7cc734b044
Gabriel St Jean <gjs19225@uga.edu>
2022-02-23 20:17:37 -0500

    Created a camera calibration program
    program is used for tuning the apperature and focal lenght for a desired image quality

---
+++ src/PiCamCalibrate.py
@@ -0,0 +1,17 @@
+from time import sleep
+from picamera import PiCamera
+
+# This module is used for calibrating the focal length and apperature
+# of the pi camera lens for a desired image quality during runtime.
+
+camera = PiCamera()
+camera.resolution = (2592, 1944) # (1024, 768)
+camera.start_preview()
+
+#camera.rotation = 180
+
+sleep(2000)
+try:
+    camera.capture('images/foo.jpg') #Captures image and saves to path
+finally:
+    camera.close()
