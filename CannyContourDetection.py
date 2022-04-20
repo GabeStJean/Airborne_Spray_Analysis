0f0fe4843d880f2e34da80e3c1e026914c4f9a43
Gabriel St Jean <gjs19225@uga.edu>
2022-04-20 08:34:31 -0400

    CannyContourDetectoin.py computes area in microns
    The area is computed using a transfer function which converts pixels into
    micronmeters. This is achevied by using a U.S. quarter as a reference
    object.

--- CannyContourDetection.py
+++ CannyContourDetection.py
@@ -7,10 +7,11 @@ import time
 
 # Opening File
 photoPath = "C:/Users/jeans/Documents/photos/"
-img = cv2.imread(photoPath + "/teststand7.jpg")
+img = cv2.imread(photoPath + "/teststand5.jpg")
 
+#teststand4
 # Resizing
-dimension = (int(img.shape[1] * 9 / 100), int(img.shape[0] * 9 / 100))
+dimension = (int(img.shape[1] * 8 / 100), int(img.shape[0] * 8 / 100))
 img = cv2.resize(img, dimension, interpolation=cv2.INTER_AREA)
 
 # Conversion
@@ -19,8 +20,8 @@ IN_TO_MICRON = 25400
 pixelsPerIn = -1
 
 # Thresholding parameters
-minValue = 100
-maxValue = 255
+minValue = 128
+maxValue = 119
 sigma = 5 # can be only 5 or 3
 
 # Other
@@ -45,6 +46,8 @@ def findContoursCanny(imgToDrawOn, inputImage):
     global pixelsPerIn
     global iteration
     global imgBoundingBox
+    global imgContourDraw
+
 
     imgBoundingBox = imgToDrawOn.copy()
     #print("\niteration: " + str(iteration))
@@ -56,7 +59,7 @@ def findContoursCanny(imgToDrawOn, inputImage):
     if (pixelsPerIn == -1):
         pixelsPerIn = cv2.contourArea(contours[0])/US_QUARTER
 
-    for i in range(1, len(contours)):
+    for i in range(0, len(contours)):
         pixelArea = cv2.contourArea(contours[i])
 
         # Identifying shape of a contour
@@ -69,29 +72,33 @@ def findContoursCanny(imgToDrawOn, inputImage):
         upperBound = .03 * pixelsPerIn
         lowerBound = .0011 * pixelsPerIn
 
+        areaString = "{area: .2f} um"  # formating
+        areaStringIn = "{area: .2f} in"  # formating
+
         # Filter out shapes that are not a circle and other noise
-        if (pixelArea > lowerBound and pixelArea < upperBound and objectCorners > 4):
+        if (pixelArea > lowerBound and objectCorners > 4):
             # Finding the area of each contour
             transferFunction = pixelArea / pixelsPerIn * IN_TO_MICRON # Convert from pixel area into inches
             #print("i=" + str(i) + ", Pixel area : " + str(cv2.contourArea(contours[i])) +
-            #    " Area in inches: " + str(transferFunction / IN_TO_MICRON) +
-            #    " Area in microns: " + str(transferFunction) +
-            #    " corner points: " + str(objectCorners))
-            cv2.drawContours(imgToDrawOn, contours, -1, (255, 0, 0), 4) # Draw all detected contours
+            #    ", Area in inches: " + areaStringIn.format(area = transferFunction / IN_TO_MICRON) +
+            #    ", Area in microns: " + areaString.format(area = transferFunction) +
+            #    ", corner points: " + str(objectCorners))
 
             # Draw bounding boxes with the area of each object
             x, y, w, h = cv2.boundingRect(cornerPoints) # x cord, y, cord, width, and height
             cv2.rectangle(imgBoundingBox, (x,y), (x+w, y+h), (0, 255, 0), 2)
-            areaString = "{area: .2f} um"
+
             cv2.putText(imgBoundingBox, areaString.format(area = transferFunction),
                     ( int(x + (w / 2) - 10), int(y + (h / 2) - 19)), cv2.FONT_HERSHEY_COMPLEX,
                     0.5, (0, 0, 0), 0)
 
-    cv2.drawContours(imgToDrawOn, contours, -1, (255, 0, 0), 4)
+    cv2.drawContours(imgToDrawOn, contours, -1, (255, 0, 0), 4) # Draw all contours
+    imgContourDraw = imgToDrawOn.copy()
     iteration += 1
 
 def cannyTrackbar():
     global imgStackCanny
+    global img
     global gray
     global imgCanny
     global imgContourDraw
@@ -104,7 +111,7 @@ def cannyTrackbar():
     cv2.createTrackbar('maxval', "Canny Detection", maxValue, 400, updateMaxValue)
     cv2.createTrackbar('sigma', "Canny Detection", 0, 1, updateSigma)
     while (1):
-        imgStackCanny = imageFormater.stackImages(0.8, ([imgContourDraw, imgCanny, imgBoundingBox]))
+        imgStackCanny = imageFormater.stackImages(1, ([imgContourDraw, imgBoundingBox]))
         cv2.imshow("Canny Detection", imgStackCanny)
         k = cv2.waitKey(1) & 0xFF  # window refresh rate in seconds
         if k == 27:  # If the escape key is pressed, end loop
@@ -129,6 +136,6 @@ findContoursCanny(imgContourDraw, imgCanny)
 
 
 
-imgStackCanny = imageFormater.stackImages(0.8, ([imgContourDraw, imgCanny, imgBoundingBox]))
+imgStackCanny = imageFormater.stackImages(1, ([imgContourDraw, imgBoundingBox]))
 cannyTrackbar()
 cv2.destroyAllWindows()
